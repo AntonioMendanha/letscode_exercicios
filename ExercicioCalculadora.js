b72b51ed8f5b416c48070055dea356bd3846c649
AntonioMendanha <antoniomendanha@me.com>
2021-10-11 18:29:17 -0300

    Corrreções da calculadora

--- ExercicioCalculadora.js
+++ ExercicioCalculadora.js
@@ -1,68 +1,82 @@
-/* Parte 1
-//Operações
-const somar = (x ,y) => x + y;
-const subtrair = (x ,y) => x - y;
-const dividir = (x ,y) => x / y;
-const multiplicar = (x ,y) => x * y;     
-
-let equation = [];
-const enter = (x) => equation.push(x);
-
-let equationList = [];
-const inputEquation = (list) => {
-    let input = `${String(list[0])} ${list[1]} ${String(list[2])} `;
-    equationList.push(input);
-}
-
-const list = () => equationList;
-const reset = () => equationList = [];
-
-
-const equals = () => {
-    if (equation[1] === '+') {
-        return somar(equation[0], equation[2])
-    } else if (equation[1] === '-') {
-        return dividir(equation[0], equation[2])
-    } else if (equation[1] === '*') {
-        return multiplicar(equation[0], equation[2])
-    } else if (equation[1] === '/') {
-        return dividir(equation[0], equation[2])
-    }
-}
-*/
-
-// Refatorarando
-let equation = [];
-const enter = (x) => equation.push(x);
-
-let equationList = [];
-const inputEquation = (list) => {
-    let input = `${String(list[0])} ${list[1]} ${String(list[2])} `;
-    equationList.push(input);
-}
-
-const list = () => equationList;
-const reset = () => {
-    equation = [];
-    equationList = [];
+// Salva os inputs
+let getInputs = {
+    inputs: [],
+    operator: []
 };
 
-const operacao = (x, y) => {
-    if (equation[1] === '+') {
-        return x + y;
-    } else if (equation[1] === '-') {
-        return x - y;
-    } else if (equation[1] === '*') {
-        return x * y;
-    } else if (equation[1] === '/') {
-        return x / y;
-    }
-}  
+// operações autorizadas
+const operatorsList = [
+    {operator: '+'},
+    {operator: '-'},
+    {operator: '*'},
+    {operator: '/'}
+];
+// validação de operação autorizada
+const isValidOperator = (operator) => operatorsList.find(element => element.operator === operator);
+const getOperator = (operator) => {
+    isValidOperator(operator) !== undefined 
+        ? getInputs.operator.push(operator) 
+        : console.log('Invalid Operator')
+};
 
-const equals = () => {
-    equation.push(
-        String(operacao(equation[0], equation[2]))
-    );
-    inputEquation(equation);
-    equation = [];
+// Input de dados para calculadora
+let enter = (input) => {
+    !isNaN(input) 
+        ? getInputs.inputs.push(input)
+        : getOperator(input)
+        
+    console.log(getInputs)
+};
+
+// Input de dados na lista
+let sentenceList = [];
+//Limpar a lista
+const reset = () => sentenceList = [];
+
+//Mostrar lista ----->> corrigindo
+const list = () => {
+    for (let i = 0; sentenceList.length > i; i++){
+        sentenceList[i].get('operation')
+    }
 }
+
+let getSentence = (inputList) => {
+let [n1, n2] = inputList.inputs
+let [operatorSymbol] = inputList.operator
+let sentence = new Map()
+    .set('n1',inputList.inputs[0])
+    .set('n2',inputList.inputs[1])
+    .set('operatorSymbol',inputList.operator);        
+sentenceList.push(`${n1} ${operatorSymbol} ${n2}`)
+return sentence
+}
+
+let equals = () => {
+    console.log('This Calculator only does single operations (ex: number1 + number2) at the time')
+        
+    //getSentence(getInputs);
+    let sentence = new Map() 
+        .set('n1', getInputs.inputs[0])
+        .set('n2', getInputs.inputs[1])
+        .set('operatorSymbol', getInputs.operator[0]);  
+       
+    const operationHandler = new Map()
+        .set('+', (x, y) => x + y)
+        .set('-', (x, y) => x - y)
+        .set('*', (x, y) => x * y)
+        .set('/', (x, y) => x / y);
+
+    const operation = operationHandler.get(sentence.get('operatorSymbol'));
+    const operacao = (x, y) => operation(x, y);
+     
+    let result = operacao(sentence.get('n1'), sentence.get('n2')); 
+    //Insere a resposta no sentence
+    sentence.set('operation', `${sentence.get('n1')}${sentence.get('operatorSymbol')}${sentence.get('n2')}=${result}`)
+    sentenceList.push(sentence);
+    //elima a operação efetuada dos getInputs
+    getInputs.inputs.shift();   
+    getInputs.inputs.shift();   
+    getInputs.operator.shift();   
+    
+    return result
+};
