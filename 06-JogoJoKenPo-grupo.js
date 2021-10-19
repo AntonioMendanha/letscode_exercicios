0e6e5bcf24abdc6410a074b3fefddac4b89cb0c9
AntonioMendanha <antoniomendanha@me.com>
2021-10-19 00:34:13 -0300

    commit 18out

--- 06-JogoJoKenPo-grupo.js
+++ 06-JogoJoKenPo-grupo.js
@@ -6,9 +6,6 @@ class Game {
     this.match = new Map()
   }
 
-  // criar método que fale: jogar
-  // passa p jogo a info que ele jogou
-
   get newGame(){
     const message = 'O jogo Pedra, Papel e Tesoura é jogado com 2 jogadores';
     console.log(message);
@@ -36,32 +33,49 @@ class Game {
     if (match.get('optionPlayer1') !== match.get('optionPlayer2')){
       this.match = match;
       console.log('Players moves are acepted - who wins?')    
-    } else { // não permite empate
+    } else {      // não permite empate
       return 'Draw or invalid choices - please repeat'
     }
   }
+  // método que adiciona os dados quando o ganhador é o player 1
+  get winnerPlayer1() {
+    this.memoryPlayers[0].stats.Win += 1;
+    this.memoryPlayers[0].stats.Played += 1;
+    this.memoryPlayers[1].stats.Loss += 1;
+    this.memoryPlayers[1].stats.Played += 1;
+  };
+  // método que adiciona os dados quando o ganhador é o player 2
+  get winnerPlayer2() {
+    this.memoryPlayers[1].stats.Win += 1;
+    this.memoryPlayers[1].stats.Played += 1;
+    this.memoryPlayers[0].stats.Loss += 1;
+    this.memoryPlayers[0].stats.Played += 1;
+  };
 
+// método para verificar quem é o ganhador
   get verifyWinner() {
     let winner = String;
     let isGameOver = false;
     let match = this.match;
-
-    //Condições para ganhar
+  
+    //Condições para ganhar, o terceiro indice indica quem ganhou a partida
     const conditionToWin = [ 
-      ['papel', 'pedra', 'Player 1 is Winner'],
-      ['papel', 'tesoura', 'Player 2 is Winner'],
-      ['pedra', 'tesoura', 'Player 1 is Winner'],
-      ['pedra', 'papel', 'Player 2 is Winner'],
-      ['tesoura', 'papel', 'Player 1 is Winner'],
-      ['tesoura', 'pedra', 'Player 2 is Winner']
+      ['papel', 'pedra', 1],
+      ['papel', 'tesoura', 2],
+      ['pedra', 'tesoura', 1],
+      ['pedra', 'papel', 2],
+      ['tesoura', 'papel', 1],
+      ['tesoura', 'pedra', 2]
     ];
     
     // Verifica quem foi o ganhador da match
     for (let i =0; i < conditionToWin.length; i++) {
       if (match.get('optionPlayer1') === conditionToWin[i][0] && match.get('optionPlayer2') == conditionToWin[i][1]) {
-        winner = conditionToWin[i][2];
+        // o ternário chama o método ganhador
+        conditionToWin[i][2] === 1 ? this.winnerPlayer1 : this.winnerPlayer2; 
         isGameOver = true;
-        return console.log(winner);
+        //retorna o array do jogadores
+        return console.log(this.memoryPlayers);
       };
     };
   }
@@ -85,9 +99,9 @@ class Player {
   }
 }
 /** Linhas abaixo são para rodar o jogo para teste */
-//let jogo = new Game();
-//jogo.newGame
-//jogo.memoryPlayers[0].setMove('pedra')  
-//jogo.memoryPlayers[1].setMove('papel')
-//jogo.showMoves
-//jogo.verifyWinner
+let jogo = new Game();
+jogo.newGame
+jogo.memoryPlayers[0].setMove('pedra')  
+jogo.memoryPlayers[1].setMove('papel')
+jogo.showMoves
+jogo.verifyWinner
